f4798e1c82d065b48c21d5ce4715262beabbc5ba
Thiago Ribeiro <46843472+ThigaoRibeiro@users.noreply.github.com>
2020-10-23 18:44:27 -0300

    otimizando espaços

--- Source/Dono.php
+++ Source/Dono.php
@@ -1,61 +1,34 @@
 <?php
 
-interface DonoInterface
-{
+interface DonoInterface{
     public function __construct($nome, $sobreNome, $email);
-
     public function getNome();
     public function getSobreNome();
-    public function getEmail();
-
-
-
-}
-
-
-
-class Dono implements DonoInterface
-{
+    public function getEmail();}
+class Dono implements DonoInterface{
     private $nome;
     private $sobreNome;
     private $email;
-
-    /**
-     * User constructor.
-     * @param $nome
-     * @param $sobreNome
-     * @param $email
-     */
+    /**     * User constructor.*@param $nome  *@param $sobreNome *@param $email*/
     public function __construct($nome, $sobreNome, $email)
-    {
-        $this->nome = $nome;
+    {  $this->nome = $nome;
         $this->sobreNome = $sobreNome;
-        $this->email = $email;
-    }
+        $this->email = $email;}
+    /**      * @return mixed      */
 
-    /**
-     * @return mixed
-     */
     public function getNome()
-    {
-        return $this->nome;
-    }
-
+    {return $this->nome;}
     /**
      * @return mixed
      */
     public function getSobreNome()
-    {
-        return $this->sobreNome;
-    }
+    {return $this->sobreNome;}
 
     /**
      * @return mixed
      */
     public function getEmail()
-    {
-        return $this->email;
-    }
+    {return $this->email;}
 
 
 
